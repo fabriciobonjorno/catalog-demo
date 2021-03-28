76ed75c0253236535bafb9f794ac8f52c77423ce
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-03-28 14:59:30 -0400

    Config routes of dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
   namespace :dashboard do
-    get 'tax_classifications/index'
+    resources :products
+    resources :tax_classifications
+    resources :families
+    resources :groups
   end
-  namespace :dashboard do
-    get 'products/index'
-  end
-  namespace :dashboard do
-    get 'families/index'
-  end
-  namespace :dashboard do
-    get 'groups/index'
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
