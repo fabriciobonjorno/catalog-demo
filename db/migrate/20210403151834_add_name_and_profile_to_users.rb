52352a6c19991fc44416b0bc484f5a3dabe5b8f8
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-04-03 11:23:27 -0400

    Config devise and users profile

---
+++ db/migrate/20210403151834_add_name_and_profile_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameAndProfileToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :profile, :boolean, default: false
+  end
+end
