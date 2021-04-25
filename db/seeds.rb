409aae61a4080031ceef7ce8ebf9f0332e082488
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-04-25 19:06:25 -0400

    Config seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Criando Administrador"
+User.create!(name: 'Fabricio Bonjorno', email: 'admin@admin.com', password: '123456',
+password_confirmation: '123456', profile: true)
+
+puts "Criando Fabricantes"
+10.times do
+  Manufacturer.create!(
+    description: Faker::Company.name
+  )
+end
+
+puts "Criando Grupos"
+10.times do
+  Group.create!(
+    description: Faker::Commerce.material,
+    manufacturer_id: rand(1..10)
+  )
+end
+
+puts "Criando Familias"
+10.times do
+  Family.create!(
+    description: Faker::Commerce.color,
+    group_id: rand(1..10)
+  )
+end
+
+puts "Criando NCM"
+10.times do
+  TaxClassification.create!(
+    description: Faker::Commerce.promotion_code
+  )
+end
+
+puts "Criando Produtos"
+10.times do
+  Product.create!(
+    description: Faker::Commerce.material,
+    family_id: rand(1..10),
+    code: rand(1..100),
+    ean: rand(100..200),
+    dun: rand(200..300),
+    display: rand(400..500)
+  )
+end
