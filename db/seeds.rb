8ec24ab831fa3b622787f0114b76ad9e3e337329
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-04-26 09:58:38 -0400

    Adjust seeds development

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,45 +21,49 @@ Company.create!(
   site: "www.meusite.com",
 )
 
-puts "Criando Fabricantes"
-10.times do
-  Manufacturer.create!(
-    description: Faker::Company.name,
-  )
+if Rails.env.development?
+  puts "Criando Fabricantes"
+  10.times do
+    Manufacturer.create!(
+      description: Faker::Company.name,
+    )
+  end
+
+  puts "Criando Grupos"
+  10.times do
+    Group.create!(
+      description: Faker::Food.fruits,
+      manufacturer_id: rand(1..10),
+    )
+  end
+
+  puts "Criando Familias"
+  10.times do
+    Family.create!(
+      description: Faker::Name.name,
+      group_id: rand(1..10),
+    )
+  end
+
+  puts "Criando NCM"
+  10.times do
+    TaxClassification.create!(
+      description: Faker::Commerce.promotion_code,
+    )
+  end
+
+  puts "Criando Produtos"
+  10.times do
+    Product.create!(
+      description: Faker::Commerce.material,
+      family_id: rand(1..10),
+      tax_classification_id: rand(1..10),
+      code: rand(1..100),
+      ean: rand(100..200),
+      dun: rand(200..300),
+      display: rand(400..500),
+    )
+  end
+
 end
 
-puts "Criando Grupos"
-10.times do
-  Group.create!(
-    description: Faker::Food.fruits,
-    manufacturer_id: rand(1..10),
-  )
-end
-
-puts "Criando Familias"
-10.times do
-  Family.create!(
-    description: Faker::Name.name,
-    group_id: rand(1..10),
-  )
-end
-
-puts "Criando NCM"
-10.times do
-  TaxClassification.create!(
-    description: Faker::Commerce.promotion_code,
-  )
-end
-
-puts "Criando Produtos"
-10.times do
-  Product.create!(
-    description: Faker::Commerce.material,
-    family_id: rand(1..10),
-    tax_classification_id: rand(1..10),
-    code: rand(1..100),
-    ean: rand(100..200),
-    dun: rand(200..300),
-    display: rand(400..500),
-  )
-end
