49ab8acef1914b9d7f3ebdca08ee1a50c1f45c71
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-05-01 17:54:57 -0400

    Create footer and refactoring navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
-  layout 'home'
+  layout "home"
+
   def index
     @products = Product.order(:code)
+    @company = Company.first
   end
 
   def home
     @products = Product.order(:code)
+    @company = Company.first
   end
 end
