6148be5a4f5947ef7726a4ea3c1b46133e12722a
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-05-02 21:20:01 -0400

    Corrections forms and index

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,19 +2,6 @@
 
 class Company < ApplicationRecord
   has_one_attached :photo_company
-  validate :image_validation
 
-  private
-
-  def image_validation
-    if respond_to?(:image) && image.attached?
-      if image.blob.byte_size > 2_000_000
-        image.purge
-        errors[:base] << 'Tamanho maximo 2MB'
-      elsif !image.blob.content_type.starts_with?('image/png', 'image/jpeg', 'image/jpg')
-        image.purge
-        errors[:base] << 'Formato invalido use PNG, JPEG, JPG'
-      end
-    end
-  end
+  validates :photo_company, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 1..5.megabytes }
 end
