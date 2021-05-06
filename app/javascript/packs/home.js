db31df18d8ad221091f1874e269e2ce437a8c76a
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-05-05 22:49:33 -0400

    Adjust photo profile and company

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -2,21 +2,17 @@ import "./application"
 import "../src/home"
 
 $(document).ready(function () {
-    var _mwi = 991;//width mobile menu enable
-    function toggleDropdown (e) {
-      //min width window 991px - hover menu show 
-      if(window.innerWidth > _mwi) {
-          var _d = $(e.target);
-          setTimeout(function () {
-            var shouldOpen = e.type !== 'click' && _d.is(':hover');
-            _d.parents('.nav-item.dropdown').find('.dropdown-menu').toggleClass('show', shouldOpen);
-            _d.parents('.nav-item.dropdown').toggleClass('show', shouldOpen);
-            $(e.target).attr('aria-expanded', shouldOpen);
-          }, e.type === 'mouseleave' ? 30 : 0);
-       }
+  var _mwi = 991;//width mobile menu enable
+  function toggleDropdown(e) {
+    //min width window 991px - hover menu show 
+    if (window.innerWidth > _mwi) {
+      var _d = $(e.target);
+      setTimeout(function () {
+        var shouldOpen = e.type !== 'click' && _d.is(':hover');
+        _d.parents('.nav-item.dropdown').find('.dropdown-menu').toggleClass('show', shouldOpen);
+        _d.parents('.nav-item.dropdown').toggleClass('show', shouldOpen);
+        $(e.target).attr('aria-expanded', shouldOpen);
+      }, e.type === 'mouseleave' ? 30 : 0);
     }
-    
-    $(document)
-        .on('mouseenter','.nav-item.dropdown',toggleDropdown)
-        .on('mouseleave','.nav-item.dropdown.show',toggleDropdown);
+  }
 });
--- app/javascript/packs/products.js
+++ app/javascript/packs/products.js
@@ -1,62 +1,62 @@
 document.addEventListener("turbolinks:load", () => {
-  
+
   $('#productTable').DataTable();
 
-  
-  $(document.body).on("change","#product_manufacturer_id",function(){
-    getManufacturerGroups(this.value, function(manufacturerGroups) {
-    	$("#product_group_id").html(renderSelectOptions(manufacturerGroups)).trigger('change');
+
+  $(document.body).on("change", "#product_manufacturer_id", function () {
+    getManufacturerGroups(this.value, function (manufacturerGroups) {
+      $("#product_group_id").html(renderSelectOptions(manufacturerGroups)).trigger('change');
     });
   });
-  $(document.body).on("change","#manufacturer_id",function(){
-    getManufacturerGroups(this.value, function(manufacturerGroups) {
-    	$("#group_id").html(renderSelectOptions(manufacturerGroups)).trigger('change');
+  $(document.body).on("change", "#manufacturer_id", function () {
+    getManufacturerGroups(this.value, function (manufacturerGroups) {
+      $("#group_id").html(renderSelectOptions(manufacturerGroups)).trigger('change');
     });
- 
+
   });
 
-  $(document.body).on("change", "#product_group_id",function() {
-  	getGroupFamilies(this.value, function(groupFamilies) {
-  		$("#product_family_id").html(renderSelectOptions(groupFamilies));
-  	});
+  $(document.body).on("change", "#product_group_id", function () {
+    getGroupFamilies(this.value, function (groupFamilies) {
+      $("#product_family_id").html(renderSelectOptions(groupFamilies));
+    });
   });
-  $(document.body).on("change", "#group_id",function() {
-  	getGroupFamilies(this.value, function(groupFamilies) {
-  		$("#family_id").html(renderSelectOptions(groupFamilies));
-  	});
+  $(document.body).on("change", "#group_id", function () {
+    getGroupFamilies(this.value, function (groupFamilies) {
+      $("#family_id").html(renderSelectOptions(groupFamilies));
+    });
   });
 
-  $(document.body).on("change", 'input[name="tiporelatorio"]',function() {
-     
-  	 if( $('#tiporelatorio_true').is(':checked')) {
+  $(document.body).on("change", 'input[name="tiporelatorio"]', function () {
+
+    if ($('#tiporelatorio_true').is(':checked')) {
       $('#relatorio').attr('action', '/dashboard/search.pdf');
-     } else {
+    } else {
       $('#relatorio').attr('action', '/dashboard/search.csv');
-     }
+    }
   });
 
 })
 
- 
+
 
 function getManufacturerGroups(manufacturerId, callback) {
   if (manufacturerId == '') { callback([]); return; }
   $.ajax(`/dashboard/groups.json?manufacturer_id=${manufacturerId}`)
     .done(callback)
-    .fail(function() { alert("error"); });
+    .fail(function () { alert("error"); });
 }
 
 function getGroupFamilies(groupId, callback) {
   if (groupId == '') { callback([]); return; }
   $.ajax(`/dashboard/families.json?group_id=${groupId}`)
     .done(callback)
-    .fail(function() { alert("error"); });
+    .fail(function () { alert("error"); });
 }
 
 function renderSelectOptions(options) {
   var htmlOptions = ""
-  options.forEach(function(option){
-    htmlOptions += "<option value='" + option.id + "'>" + option.description  + "</option>"
+  options.forEach(function (option) {
+    htmlOptions += "<option value='" + option.id + "'>" + option.description + "</option>"
   })
   return htmlOptions
 }
