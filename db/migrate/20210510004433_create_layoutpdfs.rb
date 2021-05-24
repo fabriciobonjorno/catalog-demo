d6923794efab3b8b7ae34994705ae5534e06f4b8
Jonathan da Cruz <jonathanadcz@gmail.com>
2021-05-23 21:59:57 -0300

    Layout PDF

--- db/migrate/20210510004433_create_layoutpdfs.rb
+++ db/migrate/20210510004433_create_layoutpdfs.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 class CreateLayoutpdfs < ActiveRecord::Migration[6.1]
   def change
     create_table :layoutpdfs do |t|
       t.string :color_header
-      t.integer :type_logo
+      t.string :type_logo
     end
   end
 end
