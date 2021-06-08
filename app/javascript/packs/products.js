476a4f4eeb1a6e5bd8641787e28851c5393a132c
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-06-07 21:05:58 -0400

    Adjust upload images

--- app/javascript/packs/products.js
+++ app/javascript/packs/products.js
@@ -28,45 +28,7 @@ document.addEventListener("turbolinks:load", () => {
       $('#relatorio').attr('action', '/dashboard/search.csv');
     }
   });
-
-  $(document.body).on("change", "#browse", function () {
-    var files = this.files;
-    var errors = "";
-
-    if (!files) {
-      errors += "File upload not supported by your browser.";
-    }
-    if (files && files[0]) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        if ((/\.(png|jpeg|jpg)$/i).test(file.name)) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-
-            $('.img_prev')
-              .attr('src', e.target.result)
-              .width(300);
-          };
-
-          reader.readAsDataURL(file);
-        } else {
-          errors += file.name + "\nExtensão do arquivo inválida. São válidos apenas png, jpg e jpeg\n";
-        }
-      }
-    }
-
-    if (errors) {
-      alert(errors);
-    }
-  });
-
-  $(document.body).on("change", ".photo-upload", function (e) {
-    e.target.nextElementSibling.innerHTML = e.target.files[0].name;
-  });
-
 })
-
-
 
 function getManufacturerGroups(manufacturerId, callback) {
   if (manufacturerId == '') { callback([]); return; }
