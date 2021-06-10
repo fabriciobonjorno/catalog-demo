188ff279451aa78d4af97a0f36f0b40c668fcfd7
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-06-09 23:16:00 -0400

    Adjuste turbolinks upload image

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -10,37 +10,39 @@ import "./products"
 import './addfields'
 import './removefields'
 
-  $(document.body).on("change", "#browse", function () {
-    var files = this.files;
-    var errors = "";
+document.addEventListener("turbolinks:load", () => {
+    $(document.body).on("change", "#browse", function() {
+        var files = this.files;
+        var errors = "";
 
-    if (!files) {
-      errors += "File upload not supported by your browser.";
-    }
-    if (files && files[0]) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        if ((/\.(png|jpeg|jpg)$/i).test(file.name)) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-
-            $('.img_prev')
-              .attr('src', e.target.result)
-              .width(300);
-          };
-
-          reader.readAsDataURL(file);
-        } else {
-          errors += file.name + "\nExtensão do arquivo inválida. São válidos apenas png, jpg e jpeg\n";
+        if (!files) {
+            errors += "File upload not supported by your browser.";
         }
-      }
-    }
+        if (files && files[0]) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                if ((/\.(png|jpeg|jpg)$/i).test(file.name)) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
 
-    if (errors) {
-      alert(errors);
-    }
-  });
+                        $('.img_prev')
+                            .attr('src', e.target.result)
+                            .width(300);
+                    };
 
-  $(document.body).on("change", ".photo-upload", function (e) {
-    e.target.nextElementSibling.innerHTML = e.target.files[0].name;
-  });
+                    reader.readAsDataURL(file);
+                } else {
+                    errors += file.name + "\nExtensão do arquivo inválida. São válidos apenas png, jpg e jpeg\n";
+                }
+            }
+        }
+
+        if (errors) {
+            alert(errors);
+        }
+    });
+
+    $(document.body).on("change", ".photo-upload", function(e) {
+        e.target.nextElementSibling.innerHTML = e.target.files[0].name;
+    });
+})
